217bdfb4dc11024242f9f4abac9bb5919923a572
SpaceTeleport <naivesimple@outlook.com>
2022-03-28 02:30:18 +0800

    add melting mechanic.

--- src/main/java/com/github/slarper/common/item/IcarusWings.java
+++ src/main/java/com/github/slarper/common/item/IcarusWings.java
@@ -5,13 +5,12 @@ import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ElytraItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Wearable;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.LightType;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
 
@@ -49,5 +48,41 @@ public class IcarusWings extends Item implements FabricElytraItem {
         }
 
         return false;
+    }
+
+    @Override
+    public void doVanillaElytraTick(LivingEntity entity, ItemStack chestStack) {
+        int nextRoll = entity.getRoll() + 1;
+
+        if (!entity.world.isClient && nextRoll % 10 == 0) {
+            if ((nextRoll / 10) % 2 == 0) {
+                int damageAmount;
+
+                World world = entity.getWorld();
+                long timeOfDay = world.getTimeOfDay();
+                int sunLevel = getSunLevel(timeOfDay);
+                int skyLight = world.getLightLevel(LightType.SKY, entity.getBlockPos());
+
+                damageAmount = 1 + getMeltLevel(sunLevel, skyLight);
+
+                chestStack.damage(damageAmount, entity, p -> p.sendEquipmentBreakStatus(EquipmentSlot.CHEST));
+            }
+
+            entity.emitGameEvent(GameEvent.ELYTRA_FREE_FALL);
+        }
+    }
+
+    public int getSunLevel(long timeOfDay){
+        if (timeOfDay >= 1000 && timeOfDay < 6000) {
+            return (int) (timeOfDay * 15 / 5000);
+        } else if ( timeOfDay>=6000 && timeOfDay < 11000){
+            return (int) ((10000 - timeOfDay) * 15 / 5000);
+        } else {
+            return 0;
+        }
+    }
+
+    public int getMeltLevel(int sunLevel, int skyLight){
+        return sunLevel * skyLight / 15 / 3;
     }
 }
--- src/main/java/com/github/slarper/mixin/LivingEntityMixin.java
+++ src/main/java/com/github/slarper/mixin/LivingEntityMixin.java
@@ -1,6 +1,5 @@
 package com.github.slarper.mixin;
 
-import net.fabricmc.fabric.api.entity.event.v1.FabricElytraItem;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
@@ -13,11 +12,10 @@ import static com.github.slarper.common.item.RegistryItems.ICARUS_WINGS;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-    @Inject(method = "getPreferredEquipmentSlot",at = @At(value = "HEAD"),cancellable = true)
+    @Inject(method = "getPreferredEquipmentSlot",at = @At(value = "HEAD"), cancellable = true)
     private static void getPreferredEquipmentSlot0(ItemStack stack, CallbackInfoReturnable<EquipmentSlot> cir){
         if (stack.isOf(ICARUS_WINGS)){
             cir.setReturnValue(EquipmentSlot.CHEST);
         }
-        cir.cancel();
     }
 }
