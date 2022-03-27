ac969b687b96650b9188b1b465c9043243e4ccac
SpaceTeleport <naivesimple@outlook.com>
2022-03-27 21:05:33 +0800

    Update README.md and add a temporary item model and texture for icarus_wings.

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Icarus Wings
 
+*Since ancient times, people have dreamed of being able to fly like a bird.*
+
 ## Background
 
 I want to fly in Eden Ring world ([Eden Ring is a fabric mod](https://github.com/paulevsGitch/EdenRing)) but the Elytra is too rare.
 
 So I would like to add an equip which has the same funtionality.
 
-I take inspiration from ancient Greek mythology,
+I take inspiration from ancient Greek mythology, *Wings of Icarus*.
 
-in my imagination, that is a wing crafting by Feather and Honeycomb.
+In my imagination, that is a wing crafting by Feather and Honeycomb.
--- src/main/java/com/github/slarper/common/ModItems.java
+++ src/main/java/com/github/slarper/common/ModItems.java
@@ -10,12 +10,9 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
 
     public static final Item ICARUS_WINGS;
-    public static final Item TURTLE_LEGGINgS;
 
     static {
         ICARUS_WINGS = Registry.register(Registry.ITEM, new Identifier(IcarusWingsMain.MODID, "icarus_wings"), (Item)(new ElytraItem((new Item.Settings()).maxDamage(432).group(ItemGroup.TRANSPORTATION).rarity(Rarity.UNCOMMON))));
-
-        TURTLE_LEGGINgS = Registry.register(Registry.ITEM, new Identifier(IcarusWingsMain.MODID, "turtle_leggings"), (Item)(new ArmorItem(ArmorMaterials.TURTLE, EquipmentSlot.LEGS, new Item.Settings().group(ItemGroup.COMBAT))));
 
     }
 
